8b5bc9e8bee7de3f33803b842d54f8baf07a51be
Vignesh P <vignesh.p1110@gmail.com>
2020-08-07 23:23:49 +0530

    adding netflix project using cookiecutter

---
+++ netflix/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Test data science project for netflix data',
+    author='p33ves',
+    license='MIT',
+)
